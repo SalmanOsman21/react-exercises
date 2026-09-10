f8539690c96411b2e9effae00a08c0e4430b50a6
SalmanOsman21 <salmanosman281@gmail.com>
2026-09-10 19:44:00 +0300

    Exercise 3

---
+++ Exercise 3/src/App.jsx
@@ -0,0 +1,23 @@
+import UserCard from "./UserCard"
+
+function App (){
+  return(
+
+    <>
+      <UserCard name = {"Salman"}
+      email = {"salman21@gmail.com"}
+      />
+      <UserCard name = {"Abdi"}
+      email = {"Abdin21@gmail.com"}
+      />
+      <UserCard name = {"moha"}
+      email = {"moha21@gmail.com"}
+      />
+    </>
+    
+    
+    
+  )
+}
+
+export default App;
---
+++ Exercise 3/src/UserCard.jsx
@@ -0,0 +1,11 @@
+const UserCard=({name, email})=>{
+
+    return(
+    <>
+        <h2>Name is {name}</h2>
+        <h2>Email is :{email}</h2>
+    </>
+    )
+}
+
+export default UserCard;
